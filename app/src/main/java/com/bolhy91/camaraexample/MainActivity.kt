130ef1c4a22a8e7dc831e363c63dad5114eb6ee5
bolhy91 <bolhy91@gmail.com>
2022-07-04 23:42:46 -0500

    Capturar foto desde la galeria

--- app/src/main/java/com/bolhy91/camaraexample/MainActivity.kt
+++ app/src/main/java/com/bolhy91/camaraexample/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.bolhy91.camaraexample
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.bolhy91.camaraexample.databinding.ActivityMainBinding
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +43,65 @@ class MainActivity : AppCompatActivity() {
             }
             openCamera.launch(intent)
         }
+
+        binding.saveImage.setOnClickListener {
+            saveToGallery()
+        }
+        binding.galleryImage.setOnClickListener {
+            val intent = Intent().also {
+                it.type = "image/*"
+                it.action = Intent.ACTION_GET_CONTENT
+            }
+            getImageGallery.launch(intent)
+        }
+    }
+
+    private val getImageGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data?.data
+                binding.imageCapture.setImageURI(data)
+            }
+        }
+
+    private fun saveToGallery() {
+        //crear un contenedor --> Nos indica como se llama la imagen y donde se guardara
+        val content = createContent()
+        // guardar la imagen
+        val uri = saveImage(content)
+        // limpiar la imagen
+        clearContent(content, uri)
+        Toast.makeText(this, "Imagen guardada en la galeria", Toast.LENGTH_LONG).show()
+    }
+
+    private fun saveImage(content: ContentValues): Uri {
+        var outputStream: OutputStream?
+        var uri: Uri?
+        application.contentResolver.also { resolver ->
+            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, content)
+            outputStream = resolver.openOutputStream(uri!!)
+        }
+        outputStream.use {
+            getBitMap().compress(Bitmap.CompressFormat.JPEG, 100, it)
+        }
+        return uri!!
+    }
+
+    private fun createContent(): ContentValues {
+        val fileName = file.name
+        val fileType = "image/jpg"
+        return ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.Files.FileColumns.MIME_TYPE, fileType)
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            put(MediaStore.MediaColumns.IS_PENDING, 1)
+        }
+    }
+
+    private fun clearContent(content: ContentValues, uri: Uri) {
+        content.clear()
+        content.put(MediaStore.MediaColumns.IS_PENDING, 0)
+        contentResolver.update(uri, content, null, null)
     }
 
     private lateinit var file: File
@@ -50,9 +115,13 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 //val data = result.data!!
                 //val bitmap = data.extras!!.get("data") as Bitmap
-                val bitmap = BitmapFactory.decodeFile(file.toString())
+                val bitmap = getBitMap()
                 binding.imageCapture.setImageBitmap(bitmap)
             }
         }
 
+
+    private fun getBitMap(): Bitmap {
+        return BitmapFactory.decodeFile(file.toString())
+    }
 }
